459cc7094ae00166f8afa70801b611ab97b84157
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-23 13:25:12 -0700

    Initial Commit

---
+++ MafiaMiniGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MafiaMiniGame
+//
+//  Created by Shahin on 2015-03-23.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
